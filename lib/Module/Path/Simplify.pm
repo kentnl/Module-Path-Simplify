e10609837929d55e193fd88451bae668ba0dded4
Kent Fredric <kentfredric@gmail.com>
2015-04-14 10:59:37 +1200

    initial commit

---
+++ lib/Module/Path/Simplify.pm
@@ -0,0 +1,13 @@
+use 5.006;    # our
+use strict;
+use warnings;
+
+package Module::Path::Simplify;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT: Simplify absolute paths for pretty-printing.
+
+# AUTHORITY
+
+1;
