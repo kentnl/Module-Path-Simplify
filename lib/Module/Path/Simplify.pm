37bd63a62b0d9c6a09694134bdca88f4db5230d9
Kent Fredric <kentfredric@gmail.com>
2015-04-14 14:52:28 +1200

    Add Config based reporting

--- lib/Module/Path/Simplify.pm
+++ lib/Module/Path/Simplify.pm
@@ -17,8 +17,16 @@ sub new {
 
 sub simplify {
   my ( $self, $path ) = @_;
-  my $result = $self->_find_inc($path);
-  return $self->_format_simplified($result) if defined $result;
+
+  my ( $best, ) = sort { length $a->{relative_path} <=> length $b->{relative_path} } grep { defined }
+    $self->_find_config($path),
+    $self->_find_inc($path);
+
+  if ( defined $best ) {
+    $self->aliases->set( $best->{alias}, $best->{alias_path}, $best->{display}, );
+    my $real_display = $self->aliases->get_display( $best->{alias} );
+    return sprintf q[%s/%s], $real_display, $best->{relative_path};
+  }
   return $path;
 }
 
@@ -30,6 +38,52 @@ sub aliases {
 sub pp_aliases {
   my ($self) = @_;
   return qq[\t] . join qq[\n\t], $self->aliases->pretty;
+}
+
+sub _find_config {
+  my ( undef, $path, ) = @_;
+  require Config;
+  my (@try) = (
+    { display => 'SS', key => 'sitelib_stem' },
+    { display => 'SP', key => 'siteprefix' },
+    { display => 'VS', key => 'vendorlib_stem' },
+    { display => 'VP', key => 'vendorprefix' },
+    { display => 'PP', key => 'prefix' },
+    { display => 'SA', key => 'sitearch' },
+    { display => 'SL', key => 'sitelib' },
+    { display => 'VA', key => 'vendorarch' },
+    { display => 'VL', key => 'vendorlib' },
+    { display => 'PA', key => 'archlib' },
+    { display => 'PL', key => 'privlib' },
+  );
+  my $shortest;
+  my $lib;
+  my $alias;
+  my $display;
+
+  for my $job (@try) {
+    ## no critic (Variables::ProhibitPackageVars)
+    my $candidate_lib = $Config::Config{ $job->{key} };
+    next if not defined $candidate_lib or ref $candidate_lib;
+    $candidate_lib =~ s{ /? \z }{/}gxs;
+    if ( $path =~ / \A \Q$candidate_lib\E (.*\z) /sx ) {
+      my $short = $1;
+      if ( not defined $shortest or length $short < length $shortest ) {
+        $shortest = $short;
+        $lib      = $candidate_lib;
+        $alias    = 'config.' . $job->{key};
+        ## no critic (ValuesAndExpressions::RequireInterpolationOfMetachars)
+        $display = '${' . $job->{display} . '}';
+      }
+    }
+  }
+  return unless defined $shortest;
+  return {
+    alias         => $alias,
+    relative_path => $shortest,
+    display       => $display,
+    alias_path    => $lib,
+  };
 }
 
 sub _find_inc {
